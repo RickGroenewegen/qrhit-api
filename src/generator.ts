f9d763831f56cb6fc0d537240403e71faf7c0b06
Rick Groenewegen <west14@gmail.com>
2024-09-23 16:07:33 +0200

    refactor: improve code readability and structure in generator.ts

--- src/generator.ts
+++ src/generator.ts
@@ -26,8 +26,14 @@ class Generator {
   private pdf = new PDF();
   private order = Order.getInstance();
 
-  public async generate(paymentId: string, ip: string, mollie: Mollie): Promise<void> {
-    this.logger.log(blue.bold(`Starting generation for payment: ${white.bold(paymentId)}`));
+  public async generate(
+    paymentId: string,
+    ip: string,
+    mollie: Mollie
+  ): Promise<void> {
+    this.logger.log(
+      blue.bold(`Starting generation for payment: ${white.bold(paymentId)}`)
+    );
 
     const paymentStatus = await mollie.checkPaymentStatus(paymentId);
     const userId = paymentStatus.data.payment.user.userId;
@@ -37,7 +43,9 @@ class Generator {
 
     // Check if the user is the same as the one who made the payment
     if (user.userId !== userId) {
-      this.logger.log(color.red.bold('User is not the same as the one who made the payment'));
+      this.logger.log(
+        color.red.bold('User is not the same as the one who made the payment')
+      );
       return;
     }
 
@@ -49,125 +57,26 @@ class Generator {
     // Get all playlists associated with the payment
     const playlists = await this.data.getPlaylistsByPaymentId(paymentId);
 
+    console.log(111, payment);
+    console.log(222, playlists);
+
     for (const playlist of playlists) {
       await this.generatePDF(payment, playlist, ip);
-    }
-
-    this.logger.log(color.green.bold(`Order processed successfully for payment: ${white.bold(paymentId)}`));
-  }
-
-  private async generatePDF(payment: any, playlist: any, ip: string): Promise<void> {
-    let filename = '';
-    let filenameDigital = '';
-
-    const hash = crypto.createHmac('sha256', process.env['PLAYLIST_SECRET']!)
-      .update(playlist.playlistId)
-      .digest('hex');
-
-    filename = sanitizeFilename(`${hash}_printer.pdf`.replace(/ /g, '_')).toLowerCase();
-    filenameDigital = sanitizeFilename(`${hash}_digital.pdf`.replace(/ /g, '_')).toLowerCase();
-
-    let exists = false;
-    if (payment.orderType.name === 'digital') {
-      const digitalPath = `${process.env['PUBLIC_DIR']}/pdf/${filenameDigital}`;
-      try {
-        await fs.access(digitalPath);
-        exists = true;
-        this.logger.log(color.yellow.bold(`Digital PDF already exists: ${color.white.bold(filenameDigital)}`));
-      } catch (error) {
-        // Digital file doesn't exist
-      }
-    } else {
-      const normalPath = `${process.env['PUBLIC_DIR']}/pdf/${filename}`;
-      const digitalPath = `${process.env['PUBLIC_DIR']}/pdf/${filenameDigital}`;
-      try {
-        await Promise.all([fs.access(normalPath), fs.access(digitalPath)]);
-        exists = true;
-        this.logger.log(color.yellow.bold(`Both PDFs already exist: ${color.white.bold(filename)} and ${color.white.bold(filenameDigital)}`));
-      } catch (error) {
-        // At least one of the files doesn't exist
-      }
-    }
-
-    let cardType = 'fysieke';
-    let orderName = `${payment.fullname} (${payment.countrycode})`;
-    if (payment.orderType.name === 'digital') {
-      cardType = 'digitale';
-      orderName = `${payment.fullname}`;
-    }
-
-    // Pushover
-    this.pushover.sendMessage({
-      title: `KA-CHING! € ${payment.orderType.amount.toString().replace('.', ',')} verdiend!`,
-      message: `${orderName} heeft ${payment.numberOfTracks} ${cardType} kaarten besteld voor € ${payment.totalPrice.toString().replace('.', ',')}. Playlist: ${playlist.name}`,
-      sound: 'incoming',
-    }, ip);
-
-    this.logger.log(blue.bold(`Retrieving tracks for playlist: ${white.bold(playlist.playlistId)}`));
-
-    if (playlist.resetCache) {
-      exists = false;
-      this.logger.log(color.yellow.bold(`User has refreshed the playlist cache for playlist: ${white.bold(playlist.playlistId)} so we are regenerating the PDFs`));
-    }
-
-    if (!exists) {
-      // Retrieve the tracks from Spotify
-      const response = await this.spotify.getTracks(playlist.playlistId);
-      const tracks = response.data;
-
-      // If there are more than 500 remove the last tracks
-      if (tracks.length > 500) {
-        tracks.splice(500);
-      }
-
-      this.logger.log(blue.bold(`Storing ${white.bold(tracks.length)} tracks for playlist: ${white.bold(playlist.playlistId)}`));
-      await this.data.storeTracks(playlist.id, tracks);
-
-      this.logger.log(blue.bold(`Retrieving ${white.bold(tracks.length)} tracks for playlist: ${white.bold(playlist.playlistId)}`));
-      const dbTracks = await this.data.getTracks(playlist.id);
-      playlist.numberOfTracks = dbTracks.length;
-
-      this.logger.log(blue.bold(`Creating QR codes for ${white.bold(tracks.length)} tracks for playlist: ${white.bold(playlist.playlistId)}`));
-
-      // Loop through the tracks and create a QR code for each track
-      for (const track of dbTracks) {
-        const link = `${process.env['API_URI']}/qr/${track.id}`;
-
-        // Get the first 3 characters of the track id
-        const startChars = track.trackId.substring(0, 4);
-        const outputDir = `${process.env['PUBLIC_DIR']}/qr/${startChars}`;
-        const outputPath = `${outputDir}/${track.trackId}.png`;
-        await this.utils.createDir(outputDir);
-        await this.qr.generateQR(link, outputPath);
-
-        // Create a progress based on 70-90% of the total tracks
-        const progress = Math.floor((tracks.indexOf(track) / tracks.length) * 20 + 70);
-      }
-
-      this.logger.log(blue.bold(`Creating PDF tracks for playlist: ${white.bold(playlist.playlistId)}`));
-
-      const [generatedFilenameDigital, generatedFilename] = await Promise.all([
-        this.pdf.generatePDF(filenameDigital, playlist, payment, 'digital', 80, 89),
-        payment.orderType.name != 'digital'
-          ? this.pdf.generatePDF(filename, playlist, payment, 'printer', 90, 99)
-          : Promise.resolve(''),
-      ]);
-
-      filename = generatedFilename;
-      filenameDigital = generatedFilenameDigital;
     }
 
     let printApiOrderId = '';
     let printApiOrderRequest = '';
     let printApiOrderResponse = '';
 
-    if (payment.orderType.name != 'digital') {
-      payment.printerPageCount = await this.utils.countPdfPages(`${process.env['PUBLIC_DIR']}/pdf/${filename}`);
-      const orderData = await this.order.createOrder(payment, filename);
-      printApiOrderId = orderData.response.id;
-      printApiOrderRequest = JSON.stringify(orderData.request);
-      printApiOrderResponse = JSON.stringify(orderData.response);
-    }
+    // if (playlist.orderType != 'digital') {
+    //   payment.printerPageCount = await this.utils.countPdfPages(
+    //     `${process.env['PUBLIC_DIR']}/pdf/${filename}`
+    //   );
+    //   const orderData = await this.order.createOrder(payment, filename);
+    //   printApiOrderId = orderData.response.id;
+    //   printApiOrderRequest = JSON.stringify(orderData.request);
+    //   printApiOrderResponse = JSON.stringify(orderData.response);
+    // }
 
     // Update the payment with the order id
     await this.prisma.payment.update({
@@ -181,9 +90,202 @@ class Generator {
       },
     });
 
-    await this.mail.sendEmail(payment.orderType.name, payment, playlist, filename, filenameDigital);
+    this.logger.log(
+      color.green.bold(
+        `Order processed successfully for payment: ${white.bold(paymentId)}`
+      )
+    );
+  }
 
-    this.logger.log(color.green.bold(`PDF generated successfully for playlist: ${white.bold(playlist.playlistId)}`));
+  private async generatePDF(
+    payment: any,
+    playlist: any,
+    ip: string
+  ): Promise<void> {
+    let filename = '';
+    let filenameDigital = '';
+
+    this.logger.log(
+      blue.bold(`Generating PDF for playlist: ${white.bold(playlist.name)}`)
+    );
+
+    const hash = crypto
+      .createHmac('sha256', process.env['PLAYLIST_SECRET']!)
+      .update(playlist.playlistId)
+      .digest('hex');
+
+    filename = sanitizeFilename(
+      `${hash}_printer.pdf`.replace(/ /g, '_')
+    ).toLowerCase();
+    filenameDigital = sanitizeFilename(
+      `${hash}_digital.pdf`.replace(/ /g, '_')
+    ).toLowerCase();
+
+    let exists = false;
+    if (playlist.orderType === 'digital') {
+      const digitalPath = `${process.env['PUBLIC_DIR']}/pdf/${filenameDigital}`;
+      try {
+        await fs.access(digitalPath);
+        exists = true;
+        this.logger.log(
+          color.yellow.bold(
+            `Digital PDF already exists: ${color.white.bold(filenameDigital)}`
+          )
+        );
+      } catch (error) {
+        // Digital file doesn't exist
+      }
+    } else {
+      const normalPath = `${process.env['PUBLIC_DIR']}/pdf/${filename}`;
+      const digitalPath = `${process.env['PUBLIC_DIR']}/pdf/${filenameDigital}`;
+      try {
+        await Promise.all([fs.access(normalPath), fs.access(digitalPath)]);
+        exists = true;
+        this.logger.log(
+          color.yellow.bold(
+            `Both PDFs already exist: ${color.white.bold(
+              filename
+            )} and ${color.white.bold(filenameDigital)}`
+          )
+        );
+      } catch (error) {
+        // At least one of the files doesn't exist
+      }
+    }
+
+    let cardType = 'fysieke';
+    let orderName = `${payment.fullname} (${payment.countrycode})`;
+    if (playlist.orderType === 'digital') {
+      cardType = 'digitale';
+      orderName = `${payment.fullname}`;
+    }
+
+    const profit = payment.price - payment.totalPriceWithoutTax;
+
+    // Pushover
+    this.pushover.sendMessage(
+      {
+        title: `KA-CHING! € ${profit.toString().replace('.', ',')} verdiend!`,
+        message: `${orderName} heeft ${
+          payment.numberOfTracks
+        } ${cardType} kaarten besteld voor € ${payment.totalPrice
+          .toString()
+          .replace('.', ',')}. Playlist: ${playlist.name}`,
+        sound: 'incoming',
+      },
+      ip
+    );
+
+    this.logger.log(
+      blue.bold(
+        `Retrieving tracks for playlist: ${white.bold(playlist.playlistId)}`
+      )
+    );
+
+    if (playlist.resetCache) {
+      exists = false;
+      this.logger.log(
+        color.yellow.bold(
+          `User has refreshed the playlist cache for playlist: ${white.bold(
+            playlist.playlistId
+          )} so we are regenerating the PDFs`
+        )
+      );
+    }
+
+    if (!exists) {
+      // Retrieve the tracks from Spotify
+      const response = await this.spotify.getTracks(playlist.playlistId);
+      const tracks = response.data;
+
+      // If there are more than 500 remove the last tracks
+      if (tracks.length > 500) {
+        tracks.splice(500);
+      }
+
+      this.logger.log(
+        blue.bold(
+          `Storing ${white.bold(
+            tracks.length
+          )} tracks for playlist: ${white.bold(playlist.playlistId)}`
+        )
+      );
+      await this.data.storeTracks(playlist.id, tracks);
+
+      this.logger.log(
+        blue.bold(
+          `Retrieving ${white.bold(
+            tracks.length
+          )} tracks for playlist: ${white.bold(playlist.playlistId)}`
+        )
+      );
+      const dbTracks = await this.data.getTracks(playlist.id);
+      playlist.numberOfTracks = dbTracks.length;
+
+      this.logger.log(
+        blue.bold(
+          `Creating QR codes for ${white.bold(
+            tracks.length
+          )} tracks for playlist: ${white.bold(playlist.playlistId)}`
+        )
+      );
+
+      // Loop through the tracks and create a QR code for each track
+      for (const track of dbTracks) {
+        const link = `${process.env['API_URI']}/qr/${track.id}`;
+
+        // Get the first 3 characters of the track id
+        const startChars = track.trackId.substring(0, 4);
+        const outputDir = `${process.env['PUBLIC_DIR']}/qr/${startChars}`;
+        const outputPath = `${outputDir}/${track.trackId}.png`;
+        await this.utils.createDir(outputDir);
+        await this.qr.generateQR(link, outputPath);
+
+        // Create a progress based on 70-90% of the total tracks
+        const progress = Math.floor(
+          (tracks.indexOf(track) / tracks.length) * 20 + 70
+        );
+      }
+
+      this.logger.log(
+        blue.bold(
+          `Creating PDF tracks for playlist: ${white.bold(playlist.playlistId)}`
+        )
+      );
+
+      const [generatedFilenameDigital, generatedFilename] = await Promise.all([
+        this.pdf.generatePDF(
+          filenameDigital,
+          playlist,
+          payment,
+          'digital',
+          80,
+          89
+        ),
+        playlist.orderType != 'digital'
+          ? this.pdf.generatePDF(filename, playlist, payment, 'printer', 90, 99)
+          : Promise.resolve(''),
+      ]);
+
+      filename = generatedFilename;
+      filenameDigital = generatedFilenameDigital;
+    }
+
+    await this.mail.sendEmail(
+      payment.orderType.name,
+      payment,
+      playlist,
+      filename,
+      filenameDigital
+    );
+
+    this.logger.log(
+      color.green.bold(
+        `PDF generated successfully for playlist: ${white.bold(
+          playlist.playlistId
+        )}`
+      )
+    );
   }
 }
 
