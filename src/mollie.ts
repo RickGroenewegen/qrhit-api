2ace124becac8fb3077ad276f9ba0867a3375d65
Rick Groenewegen <west14@gmail.com>
2024-09-25 14:44:47 +0200

    refactor: improve formatting and readability of getPaymentList method in Mollie class

--- src/mollie.ts
+++ src/mollie.ts
@@ -35,11 +35,16 @@ class Mollie {
     return (localeMap[locale] || 'en_US') as Locale; // Default to en_US if no match is found
   }
 
-  public async getPaymentList(search: OrderSearch & { page: number, itemsPerPage: number }): Promise<{ payments: any[], totalItems: number }> {
-    const whereClause = Array.isArray(search.status) && search.status.length > 0 ? { status: { in: search.status } } : {};
+  public async getPaymentList(
+    search: OrderSearch & { page: number; itemsPerPage: number }
+  ): Promise<{ payments: any[]; totalItems: number }> {
+    const whereClause =
+      Array.isArray(search.status) && search.status.length > 0
+        ? { status: { in: search.status } }
+        : {};
 
     const totalItems = await this.prisma.payment.count({
-      where: whereClause
+      where: whereClause,
     });
 
     const payments = await this.prisma.payment.findMany({
