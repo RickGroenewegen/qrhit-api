02d8853bdc65c2fdfed830bb522fa601a1e5e3ac
Rick Groenewegen <west14@gmail.com>
2024-10-06 10:41:08 +0200

    25 invisible

--- src/order.ts
+++ src/order.ts
@@ -132,6 +132,9 @@ class Order {
           maxCards: true,
           amountWithMargin: true,
         },
+        where: {
+          visible: true,
+        },
         orderBy: [
           {
             digital: 'desc',
