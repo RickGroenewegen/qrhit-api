de2d4bc42f2157bb55c73405f04ad051497806e6
Rick Groenewegen <west14@gmail.com>
2024-10-10 10:09:24 +0200

    f

--- src/mail.ts
+++ src/mail.ts
@@ -387,7 +387,11 @@ ${attachment.data}
 
     const rawEmail = `From: ${params.from}
 To: ${params.to}
-${process.env['ENVIRONMENT'] !== 'development' ? `Bcc: ${process.env['INFO_EMAIL']!}` : ''}
+${
+  process.env['ENVIRONMENT'] !== 'development'
+    ? `Bcc: ${process.env['INFO_EMAIL']!}`
+    : ''
+}
 Subject: ${params.subject}
 ${replyToHeader}MIME-Version: 1.0
 Content-Type: multipart/mixed; boundary="MixedBoundaryString"
