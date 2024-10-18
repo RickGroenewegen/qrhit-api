7774b27848c36349a476e800fd6c9faa4ebb87a9
Rick Groenewegen <west14@gmail.com>
2024-10-18 11:22:00 +0200

    test

--- src/git.ts
+++ src/git.ts
@@ -2,13 +2,16 @@ import { CronJob } from 'cron';
 import { exec } from 'child_process';
 import Utils from './utils';
 import cluster from 'cluster';
+import Logger from './logger';
+import { color } from 'console-log-colors';
 
 class GitChecker {
   private static instance: GitChecker;
   private utils = new Utils();
+  private logger = new Logger();
 
   private constructor() {
-    if (cluster.isPrimary) {
+    if (cluster.isPrimary && process.env.NODE_ENV === 'production') {
       this.startCronJob();
     }
   }
@@ -20,38 +23,45 @@ class GitChecker {
     return GitChecker.instance;
   }
 
-  private startCronJob() {
-    const job = new CronJob('*/1 * * * *', () => {
-      this.checkForChanges();
+  private async startCronJob() {
+    const job = new CronJob('*/1 * * * *', async () => {
+      await this.checkForChanges();
     });
     job.start();
   }
 
-  private checkForChanges() {
+  private async checkForChanges() {
     exec('git fetch && git status -uno', (error, stdout) => {
       if (error) {
-        console.error(`Error executing git command: ${error.message}`);
+        this.logger.log(
+          color.red.bold(
+            `Error executing git command: ${color.white.bold(error.message)}`
+          )
+        );
         return;
       }
       if (stdout.includes('Your branch is behind')) {
-        console.log('There are new changes in the repository.');
-        exec(
-          'git reset --hard HEAD && git pull origin HEAD && pm2 restart qrsong',
-          (resetError, resetStdout) => {
-            if (resetError) {
-              console.error(
-                `Error executing reset and pull: ${resetError.message}`
-              );
-              return;
-            }
-            console.log(
-              'Repository updated and service restarted:',
-              resetStdout
-            );
-          }
+        this.logger.log(
+          color.blue.bold('There are new changes in the repository.')
         );
+        exec('deploy_qrsong', (resetError, resetStdout) => {
+          if (resetError) {
+            this.logger.log(
+              color.red.bold(
+                `Error executing reset and pull: ${color.white.bold(
+                  resetError.message
+                )}`
+              )
+            );
+            return;
+          }
+          this.logger.log(
+            color.green.bold('Repository updated and service restarted!:')
+          );
+          console.log(resetStdout);
+        });
       } else {
-        console.log('No new changes in the repository.');
+        this.logger.log(color.blue.bold('No new changes in the repository.'));
       }
     });
   }
