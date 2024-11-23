7768a7f780050d85bb7ac0b95b5ae98e4bb315ed
Rick Groenewegen <west14@gmail.com>
2024-11-23 14:01:45 +0100

    feat: Add OpenPerplex module

---
+++ src/openperplex.ts
@@ -0,0 +1,75 @@
+import Utils from './utils';
+import axios from 'axios';
+
+export class OpenPerplex {
+  private utils = new Utils();
+  private apiKey: string;
+  private baseUrl = 'https://api.perplexity.ai';
+
+  constructor() {
+    const apiKey = process.env['PERPLEXITY_API_KEY'];
+    if (!apiKey) {
+      throw new Error('PERPLEXITY_API_KEY environment variable is not defined');
+    }
+    this.apiKey = apiKey;
+  }
+
+  public async ask(artist: string, title: string): Promise<number> {
+    let year = 0;
+    const prompt = `What is the release date of the song ${title} by ${artist}. Only output the year`;
+
+    const baseUrl =
+      'https://44c57909-d9e2-41cb-9244-9cd4a443cb41.app.bhs.ai.cloud.ovh.net';
+    const apiKey = process.env['OPENPERPLEX_API_KEY']; // Replace with your actual API key
+    const options = {
+      query: prompt,
+      date_context: '2024-09-09 7:00PM',
+      location: 'us',
+      pro_mode: 'false',
+      response_language: 'en',
+      answer_type: 'text',
+      search_type: 'general',
+      verbose_mode: 'false',
+      return_sources: 'false',
+      return_images: 'false',
+      return_citations: 'false',
+      recency_filter: 'anytime',
+    };
+    const params = new URLSearchParams(
+      Object.fromEntries(
+        Object.entries(options).map(([key, value]) => [key, String(value)])
+      )
+    );
+
+    try {
+      if (!apiKey) {
+        throw new Error(
+          'OPENPERPLEX_API_KEY environment variable is not defined'
+        );
+      }
+
+      const response = await fetch(`${baseUrl}/search?${params}`, {
+        method: 'GET',
+        headers: new Headers({
+          'X-API-Key': apiKey,
+          'Content-Type': 'application/json',
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      try {
+        year = parseInt(data.llm_response, 10);
+      } catch (e) {}
+
+      return year;
+    } catch (error) {
+      console.error('Error:', error);
+      throw error;
+    }
+  }
+}
